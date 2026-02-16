00c19c117486584ea587e9cb8b31ab369481461d
fine-print <605lilnut@gmail.com>
2026-02-16 12:49:25 -0600

    Update App.jsx
    added GameProvider to webapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import ScenePage from "./ScenePage.jsx";
-import NavBar from "./NavBar";
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ScenePage from './ScenePage.jsx';
+import NavBar from './NavBar.jsx';
+import { GameProvider } from './game/GameProvider.jsx';
 
 export default function App() {
   return (
-    <>
-	  <NavBar />
+    <GameProvider>
+      <NavBar />
       <Routes>
-        {/* Main pages driven by uiMap.json */}
         <Route path="/" element={<ScenePage pageKey="landing" />} />
-        <Route path="/marketplace" element={<ScenePage pageKey="marketplace" />} />
-
-        {/* Placeholders for now (we'll build these scenes next) */}
-        <Route path="/referrals" element={<ScenePage pageKey="referrals" />} />
-        <Route path="/breeding" element={<ScenePage pageKey="breeding" />} />
-        <Route path="/bpacks" element={<ScenePage pageKey="bpacks" />} />
+        <Route path="/lab" element={<ScenePage pageKey="lab" />} />
+        <Route path="/supply" element={<ScenePage pageKey="supply" />} />
+        <Route path="/storage" element={<ScenePage pageKey="storage" />} />
+        <Route path="/invite" element={<ScenePage pageKey="invite" />} />
         <Route path="/profile" element={<ScenePage pageKey="profile" />} />
 
-        {/* Fallback */}
-        <Route path="*" element={<div style={{ color: "white", padding: 20 }}>404 Not Found</div>} />
+        <Route path="*" element={<div style={{ color: 'white', padding: 20 }}>404 Not Found</div>} />
       </Routes>
-	</>
+    </GameProvider>
   );
 }
